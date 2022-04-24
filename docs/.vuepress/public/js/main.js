0b5b7827f9a59f8de0e20cefd974d369206518ab
想走过亚洲的熊 <1693889638@qq.com>
2022-04-24 22:59:18 +0800

    滚动条样式修改

--- docs/.vuepress/public/js/main.js
+++ docs/.vuepress/public/js/main.js
@@ -6,4 +6,8 @@ window.onload = function () {
   //   "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F113020142315%2F201130142315-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653364673&t=c00bb86519c63b71bde598789cc13bd0";
   // img.setAttribute("src", imgurl);
   // sidebar.appendChild(img);
+
+  // 给 body 加载滚动条
+  let body = document.body;
+  body.setAttribute("class", "test-5");
 };
