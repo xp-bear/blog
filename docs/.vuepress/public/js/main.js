79cb249e6b76c99db2a2de5a10df9e007deef374
想走过亚洲的熊 <1693889638@qq.com>
2022-04-25 17:13:09 +0800

    初版样式确定

--- docs/.vuepress/public/js/main.js
+++ docs/.vuepress/public/js/main.js
@@ -1,42 +1,46 @@
-// window.onload = function () {
-//   // let sidebar = document.querySelector(".sidebar");
-//   // let img = document.createElement("img");
-//   // img.setAttribute("id", "image");
-//   // imgurl =
-//   //   "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F113020142315%2F201130142315-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653364673&t=c00bb86519c63b71bde598789cc13bd0";
-//   // img.setAttribute("src", imgurl);
-//   // sidebar.appendChild(img);
-//   // 给 body 加载滚动条
-//   // let body = document.body;
-//   // body.setAttribute("class", "test-5");
-// };
-
 window.onload = function () {
   let body = document.body;
   let div = document.createElement("div");
   div.setAttribute("id", "mydiv");
   body.appendChild(div);
   // 加载音乐背景插件
-  const setAttributes = (element, attributes) => {
-    for (let key in attributes) {
-      if (Object.prototype.toString.call(attributes[key]) === "[object Object]") {
-        setAttributes(element[key], attributes[key]);
-      } else {
-        element[key] = attributes[key];
-      }
-    }
-  };
-  let iframe = document.createElement("iframe");
-  setAttributes(iframe, {
-    id: "myaudio",
-    frameborder: "no",
-    border: "0",
-    marginwidth: "0",
-    width: "260",
-    height: "80",
-    src: "http://music.163.com/outchain/player?type=2&id=28403111&auto=1&height=66",
-  });
-  body.appendChild(iframe);
+  // const setAttributes = (element, attributes) => {
+  //   for (let key in attributes) {
+  //     if (Object.prototype.toString.call(attributes[key]) === "[object Object]") {
+  //       setAttributes(element[key], attributes[key]);
+  //     } else {
+  //       element[key] = attributes[key];
+  //     }
+  //   }
+  // };
+  // let iframe = document.createElement("iframe");
+  // setAttributes(iframe, {
+  //   id: "myaudio",
+  //   frameborder: "no",
+  //   border: "0",
+  //   marginwidth: "0",
+  //   width: "260",
+  //   height: "80",
+  //   src: "http://music.163.com/outchain/player?type=2&id=28403111&auto=1&height=66",
+  // });
+  // body.appendChild(iframe);
+
+  // 添加音乐图片
+  // let img = document.createElement("img");
+  // img.setAttribute("id", "photo");
+  // img.setAttribute("src", "http://pic.xxoutman.cn/img/music.png");
+  // body.appendChild(img);
+
+  // 监听按钮点击事件
+  // let photo = document.getElementById("photo");
+  // let myaudio = document.getElementById("myaudio");
+  // photo.addEventListener("click", function (e) {
+  //   photo.id = "animationOut";
+  //   setTimeout(() => {
+  //     myaudio.style.display = "block";
+  //     myaudio.classList.add = "animationIn";
+  //   }, 300);
+  // });
 
   //背景配置粒子
   var config = {
